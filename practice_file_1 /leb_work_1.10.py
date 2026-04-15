34de1d8c2e61f44af31998dd49d1356b7d734b91
Isha Magatarapara <ishamagatarapara@gmail.com>
2026-04-15 14:30:47 +0530

    Delete practice_file_1  directory

--- practice_file_1 /leb_work_1.10.py
+++
@@ -1,3 +0,0 @@
-#Q9. Find the remainder when 17 is divided by 5 using the % operator. [Easy]
-
-print("Remainder =",17%5)
--- practice_file_1 /leb_work_1.3.py
+++
@@ -1,4 +0,0 @@
-#Q3. Take user input for a name using input() and print a greeting. [Easy]
-
-user_name =input("Enter your name: ")
-print("Hello," + user_name + "!")
