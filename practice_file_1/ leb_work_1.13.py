cfe3e739676a7ea5d6a15992fc3f3e32002a923c
Isha Magatarapara <ishamagatarapara@gmail.com>
2026-04-15 14:29:50 +0530

    Delete practice_file_1/ leb_work_1.13.py

--- practice_file_1/ leb_work_1.13.py
+++
@@ -1,7 +0,0 @@
-#Q13. Convert temperature from Celsius to Fahrenheit: F = C x 9/5 + 32. [Medium]
-
-C = float(input("Enter temureture in celsius: "))
-
-F = (C * 9/5) + 32
-
-print("Tempurature in Fahrenheit = ",F)
