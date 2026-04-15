4a0693161c56bbb09aeec79ea752fe2b2c1aecf2
Isha Magatarapara <ishamagatarapara@gmail.com>
2026-04-15 14:30:08 +0530

    Delete practice_file_1/ leb_work_1.7.py

--- practice_file_1/ leb_work_1.7.py
+++
@@ -1,6 +0,0 @@
-#Q7. Add two numbers entered by the user and display the result. [Easy]
-
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter Second number: "))
-
-print("Sum = ", num1+num2)
