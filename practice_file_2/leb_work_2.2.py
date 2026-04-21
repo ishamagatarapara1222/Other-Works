256a081e5d4b8672aca915749558fb4923ef736c
Isha Magatarapara <ishamagatarapara@gmail.com>
2026-04-21 14:33:45 +0530

    Delete practice_file_2 directory

--- practice_file_2/leb_work_2.2.py
+++
@@ -1,2 +0,0 @@
-user_input = input("Enter Something:")
-print("You enterd:",user_input)
--- practice_file_2/leb_work_2.6.py
+++
@@ -1,8 +0,0 @@
-#6. Check if a number is even or odd
-
-num = int(input("Enter a number:"))
-
-if num%2==0:
-    print("Number is Even.")
-else:
-    print("Number is odd.")
